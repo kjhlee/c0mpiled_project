ea042fa7039de296423048ea642451c43c6994d7
KJ Lee <kunnyjlee@gmail.com>
2026-02-21 15:06:04 -0800

    Created questionList and questionModel

--- backend/demo/src/main/java/com/example/demo/DemoApplication.java
+++ backend/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -3,11 +3,15 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.example.demo.enums.Difficulty;
+import com.example.demo.model.QuestionModel;
+
 @SpringBootApplication
 public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+        QuestionModel question = new QuestionModel();
     }
 
 }
---
+++ backend/demo/src/main/java/com/example/demo/model/QuestionList.java
@@ -0,0 +1,33 @@
+package com.example.demo.model;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.example.demo.model.QuestionModel;
+
+public class QuestionList {
+    
+    private List<QuestionModel> questionList = new ArrayList<>();
+
+    public QuestionList() {
+        // construct a list of Leetcode Questions
+        for (int i = 0; i < 5; i++){
+            QuestionModel question = new QuestionModel();
+            question.setId("question" + i);
+            question.setQuestion("This is question " + i);
+            question.setDifficulty(null);
+            question.setConcepts(null);
+            question.setHint1("This is your first hint");
+            question.setHint2(null);
+            question.setHint3(null);
+            questionList.add(question);
+        }
+    }
+
+    // getter for questionList
+    public List<QuestionModel> getQuestionList() {
+        return questionList;
+    }
+
+}
